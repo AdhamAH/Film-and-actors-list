e4983b19ecb6696825236fd1c087c36067df70a2
Adham Abo Hasson <development@adhamabohasson.com>
2021-11-18 19:51:52 +0100

    Replace apollo-server with apollo-server-express #12 (#13)

--- server/README.md
+++ server/README.md
@@ -7,6 +7,7 @@ then run it `npm run dev` or `yarn run dev`
 
 ## You need to add `.env` with the following as an example:
 ````dotenv
+NODE_ENV= development
 PORT = 4000
 KEY = 66556A586E3272357538782F413F4428472D4B6150645367566B597033733676
 IV = f054600ba4d142e5bd99fceff053c778
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,19 +3,27 @@ require('dotenv').config();
 import {UserResolver} from "./resolvers/UserResolver";
 import {buildSchema} from "type-graphql";
 import {createConnection} from "typeorm";
-import {ApolloServer} from "apollo-server";
 import {FilmsResolver} from "./resolvers/FilmsResolver";
-
+import express from "express";
+import {ApolloServer} from "apollo-server-express";
  async function main() {
    await createConnection()
+     const app = express();
     const schema = await buildSchema({
         resolvers: [UserResolver, FilmsResolver],
       validate:false
     })
-    const server = new ApolloServer({ schema })
-    await server.listen(process.env.PORT)
+    const apolloServer = new ApolloServer({ schema })
+    await apolloServer.start()
+     apolloServer.applyMiddleware({
+         app,
+         cors: process.env.NODE_ENV==="development",
+     });
      const PORT = process.env.PORT||4000
-    console.log(`Server has started at ${PORT}!`)
+     app.listen(PORT,()=>{
+         console.log(`Server has started at ${PORT}!`)
+     })
+
 }
 main().catch((err) => {
     console.error(err);
