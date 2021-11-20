747758339ca78371f7d8b1bb29fa26a4fd11b5ff
Adham Abo Hasson <55054963+AdhamAH@users.noreply.github.com>
2021-11-20 13:14:51 +0100

    add login and logout&& add cookie using redis and express-session

---
+++ server/src/types/myCtx.ts
@@ -0,0 +1,8 @@
+import {  Response, Request} from "express";
+interface SessionData {
+    userID: number;
+}
+export type MyCtx = {
+    req: Request & { session:SessionData };
+    res: Response;
+};
