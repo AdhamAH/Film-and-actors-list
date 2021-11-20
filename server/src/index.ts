f1f23dc7278de623aef20e0d39577e4174368485
Adham Abo Hasson <development@adhamabohasson.com>
2021-11-20 13:15:43 +0100

    add login and logout&& add cookie using redis and express-session (#14)

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,19 +6,66 @@ import {createConnection} from "typeorm";
 import {FilmsResolver} from "./resolvers/FilmsResolver";
 import express from "express";
 import {ApolloServer} from "apollo-server-express";
+import cors from 'cors'
+import redis from 'redis'
+import session from 'express-session'
+import connectRedis from 'connect-redis'
+import {
+    ApolloServerPluginLandingPageDisabled,
+    ApolloServerPluginLandingPageGraphQLPlayground
+} from "apollo-server-core";
+
+
+
+
+
  async function main() {
    await createConnection()
+     const RedisStore = connectRedis(session)
+     const redisClient = redis.createClient()
      const app = express();
-    const schema = await buildSchema({
-        resolvers: [UserResolver, FilmsResolver],
-      validate:false
+
+     app.use(
+         cors({
+             origin: ["https://studio.apollographql.com"],
+             credentials: true,
+         })
+     );
+
+     app.use(
+         session({
+             name:'token',
+             store: new RedisStore({ client: redisClient, disableTouch:true }),
+             cookie:{
+               maxAge: 1000*60*60,
+               httpOnly: true,
+                 sameSite: 'lax',
+               secure: process.env.NODE_ENV!=='development'
+             },
+             saveUninitialized: false,
+             secret: process.env.SECRET as string,
+             resave: false,
+         })
+     )
+     const schema = await buildSchema({
+         resolvers: [UserResolver, FilmsResolver],
+         validate:false
+     })
+    const apolloServer = new ApolloServer({ schema,
+    context: ({ req, res }) => ({
+        req,
+        res,
+    }),
+        plugins: [
+            ApolloServerPluginLandingPageGraphQLPlayground()
+            , ApolloServerPluginLandingPageDisabled()
+        ]
     })
-    const apolloServer = new ApolloServer({ schema })
     await apolloServer.start()
      apolloServer.applyMiddleware({
          app,
-         cors: process.env.NODE_ENV==="development",
-     });
+         cors: false,
+ });
      const PORT = process.env.PORT||4000
      app.listen(PORT,()=>{
          console.log(`Server has started at ${PORT}!`)
--- server/src/resolvers/UserResolver.ts
+++ server/src/resolvers/UserResolver.ts
@@ -1,11 +1,23 @@
-import {Resolver, Query, Arg, Mutation} from "type-graphql";
+import {Resolver, Query, Arg, Mutation, Ctx} from "type-graphql";
 import {User} from "../entities/User";
 import {UserResponse} from "../types/user.types";
 import {getRepository} from "typeorm";
 import {CreateUserInput} from "../inputs/createUserInput";
+import {MyCtx} from "../types/myCtx";
 
 @Resolver()
 export class UserResolver {
+
+    @Query(()=>User,{nullable:true})
+   async me(@Ctx(){req}:MyCtx){
+        if(!req.session.userID){
+            return null
+        } else {
+
+            return await User.findOne({id:req.session.userID})
+        }
+    }
+
     @Query(() => [UserResponse])
     users() {
         return getRepository(User).find();
@@ -40,11 +52,54 @@ export class UserResolver {
             return {user}
         }
     }
+    @Query(()=>UserResponse)
+    async logIn(@Arg("data") data:CreateUserInput,
+                @Ctx(){req}:MyCtx):Promise<UserResponse>{
+        const user = await User.findOne({username:data.username})
+        console.log(req.session)
+        if(!user){
+            return {
+                errors:[{
+                    field: 'Login',
+                    message: "The information provided is not correct, Try again please!"
+                }]
+            }
+        } else {
+            if(data.password===user.password){
+                req.session.userID=user.id
+                return {user}
+            }else{
+                return {
+                    errors:[{
+                        field: 'Login',
+                        message: "The information provided is not correct, Try again please!"
+                    }]
+                }
+            }
+        }
+    }
+    @Mutation(() => Boolean)
+    logout(@Ctx() { req, res }: MyCtx) {
+        return new Promise((resolve) =>
+            req.session.destroy((err) => {
+                res.clearCookie("token");
+                if (err) {
+                    console.log(err);
+                    resolve(false);
+                    return;
+                }
+
+                resolve(true);
+            })
+        );
+    }
     @Mutation(() => UserResponse)
-    async createUser(@Arg("data") data: CreateUserInput):Promise<UserResponse> {
+    async createUser(@Arg("data") data: CreateUserInput,
+                     @Ctx(){req}:MyCtx):Promise<UserResponse> {
         const user = User.create(data);
         try{
             await user.save();
+            req.session.userID=user.id
             return {user};
         } catch (error){
              if(error.errno===19){
