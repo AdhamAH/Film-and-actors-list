c1fd3a65b6c564b615ad3372d10f6621259840b7
Adham Abo Hasson <55054963+AdhamAH@users.noreply.github.com>
2021-11-22 10:24:54 +0100

    create basic frontend setup with register and login

---
+++ client/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export const Home = (): JSX.Element => (
+  <div>
+    <Head>
+      <title>Films And Actors List - Home</title>
+    </Head>
+
+    <main></main>
+  </div>
+)
+
+export default Home
